c58f50fded677a36125da05a9611ebefcaf9da12
Feconyh <carlosedu445c@gmail.com>
2023-02-16 15:56:32 -0300

    Hover pq Ts nao aceita Input

--- barbearia/src/app/perfil/tela-perfil/tela-perfil.page.ts
+++ barbearia/src/app/perfil/tela-perfil/tela-perfil.page.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaPerfilPage implements OnInit {
 
-  public editMode = false
+  public editMode = true
 
-  constructor() { }
+  constructor() {}
 
   edit(){
     switch (this.editMode){
